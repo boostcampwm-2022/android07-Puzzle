6ab02f6d7e099e5443097b59609f71e011713310
장주원 <rptrpt@ajou.ac.kr>
2022-11-17 16:18:07 +0900

    feat: fireRepo 반환값 수정 및 필터링 함수 추가

--- app/src/main/java/com/juniori/puzzle/data/firebase/FirebaseService.kt
+++ app/src/main/java/com/juniori/puzzle/data/firebase/FirebaseService.kt
@@ -5,28 +5,26 @@ import com.juniori.puzzle.data.firebase.dto.RunQueryRequestDTO
 import com.juniori.puzzle.data.firebase.dto.RunQueryResponseDTO
 import com.juniori.puzzle.data.firebase.dto.VideoDetail
 import com.juniori.puzzle.data.firebase.dto.VideoItem
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface FirebaseService {
-    @GET("databases/(default)/documents/video")
+    @GET("databases/(default)/documents/videoReal")
     suspend fun listVideoItemDocuments(
         @Query("pageSize") pageSize: Int,
         @Query("pageToken") pageToken: String,
         @Query("orderBy") orderBy: String
     ): ListDocumentsResponseDTO
 
-    @POST("databases/(default)/documents/video")
+    @POST("databases/(default)/documents/videoReal")
     suspend fun createVideoItemDocument(
-        @Query("documentId") videoName: String,
         @Body fields: Map<String, VideoDetail>
-    ): Response<VideoItem>
+    ): VideoItem
 
     @POST("databases/(default)/documents:runQuery")
     suspend fun getFirebaseItemByQuery(
         @Body fields: RunQueryRequestDTO
-    ): Response<List<RunQueryResponseDTO>>
+    ): List<RunQueryResponseDTO>
 }
