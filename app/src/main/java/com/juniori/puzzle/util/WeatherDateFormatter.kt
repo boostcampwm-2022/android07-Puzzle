69c24e5932689fada9731360900f8423a255819e
uuranus <hyur0920@gmail.com>
2022-11-19 20:11:30 +0900

    feat: 날씨 날짜 formatting

--- app/src/main/java/com/juniori/puzzle/util/WeatherDateFormatter.kt
+++ app/src/main/java/com/juniori/puzzle/util/WeatherDateFormatter.kt
@@ -6,27 +6,30 @@ import java.util.Calendar
 import java.util.Date
 
 @SuppressLint("SimpleDateFormat")
-private val formatter = SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
+private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 private val calendar = Calendar.getInstance()
 
 fun String.toDate(): Date? = formatter.parse(this)
 
 fun Date.toFullDate(): String {
     calendar.time = this
-    return "${calendar.get(Calendar.YEAR)}년 ${calendar.get(Calendar.MONTH)+1}월 ${calendar.get(Calendar.DATE)}일" +
-            "${calendar.get(Calendar.DAY_OF_MONTH)} ${calendar.get(Calendar.HOUR)}시 " +
-            "${calendar.get(Calendar.MINUTE)}분"
+    return String.format(
+        "%d년 %02d월 %02d일 %02d시 %02d분",
+        calendar.get(Calendar.YEAR),
+        calendar.get(Calendar.MONTH) + 1,
+        calendar.get(Calendar.DATE),
+        calendar.get(Calendar.HOUR_OF_DAY),
+        calendar.get(Calendar.MINUTE)
+    )
 }
 
 fun Date.toTime(): String {
     calendar.time = this
     val hour = calendar.get(Calendar.HOUR_OF_DAY)
-    return if (hour >= 12) {
-        if (hour == 12) {
-            "오후 ${hour}시"
-        } else {
-            "오후 ${hour - 12}시"
-        }
+    return if (hour > 12) {
+        "오후 ${hour - 12}시"
+    } else if (hour == 12) {
+        "오후 ${hour}시"
     } else {
         "오전 ${hour}시"
     }
