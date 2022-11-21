057d6e494777e20dc2bd3ff2e59c474bb53245a8
장주원 <rptrpt@ajou.ac.kr>
2022-11-21 17:42:56 +0900

    feat: 비디오 업로드 및 화면 이동 동작 연결

--- app/src/main/java/com/juniori/puzzle/ui/addvideo/upload/UploadStep2Fragment.kt
+++ app/src/main/java/com/juniori/puzzle/ui/addvideo/upload/UploadStep2Fragment.kt
@@ -6,9 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import com.juniori.puzzle.data.Resource
 import com.juniori.puzzle.databinding.FragmentUploadStep2Binding
 import com.juniori.puzzle.ui.addvideo.AddVideoViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class UploadStep2Fragment : Fragment() {
 
@@ -32,6 +38,7 @@ class UploadStep2Fragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSave.setOnClickListener {
+            viewModel.uploadVideo("${requireContext().cacheDir.path}/${viewModel.videoName.value}.mp4")
         }
         binding.buttonGoback.setOnClickListener {
             findNavController().navigateUp()
@@ -39,6 +46,32 @@ class UploadStep2Fragment : Fragment() {
         binding.containerRadiogroup.setOnCheckedChangeListener { radioGroup, checkedId ->
             if (checkedId == binding.radiobuttonSetPublic.id) {
             } else if (checkedId == binding.radiobuttonSetPrivate.id) {
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uploadFlow.collectLatest { resource ->
+                    resource?.let {
+                        when (it) {
+                            is Resource.Success -> {
+                                arguments?.let { bundle ->
+                                    findNavController().navigate(
+                                        bundle.getInt(
+                                            "previousFragment"
+                                        )
+                                    )
+                                }
+                            }
+                            is Resource.Failure -> {
+                                /** upload video가 실패했을때의 ui 처리 */
+                            }
+                            is Resource.Loading -> {
+                                /** video upload 중일때의 ui 처리 */
+                            }
+                        }
+                    }
+                }
             }
         }
     }
