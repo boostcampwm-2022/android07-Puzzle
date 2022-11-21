2ba79d4a435e9b55a3a578f99497806a7dac5eb4
K_Gs:D <rootachieve6053@gmail.com>
2022-11-21 21:05:00 +0900

    feat : 뷰모델에 검색 구현

--- app/src/main/java/com/juniori/puzzle/ui/mygallery/MyGalleryViewModel.kt
+++ app/src/main/java/com/juniori/puzzle/ui/mygallery/MyGalleryViewModel.kt
@@ -4,44 +4,59 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.juniori.puzzle.data.Resource
+import com.juniori.puzzle.domain.entity.VideoInfoEntity
 import com.juniori.puzzle.domain.usecase.GetMyVideoListUseCase
-import com.juniori.puzzle.domain.usecase.GetSearchedMyVideoUseCase
+import com.juniori.puzzle.domain.usecase.GetUserInfoUseCase
+import com.juniori.puzzle.mock.getVideoListMockData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.*
-import kotlinx.coroutines.GlobalScope.coroutineContext
 import javax.inject.Inject
 
 @HiltViewModel
 class MyGalleryViewModel @Inject constructor(
     val getMyVideoListUseCase: GetMyVideoListUseCase,
-    val getSearchedMyVideoUseCase: GetSearchedMyVideoUseCase
+    val getUserInfoUseCase: GetUserInfoUseCase
 ) : ViewModel() {
-    private val _list = MutableLiveData(VideoMockData.mockList(0))
-    val list: LiveData<List<VideoMockData>>
+    private var myData = listOf<VideoInfoEntity>()
+
+    private val _list = MutableLiveData<List<VideoInfoEntity>>()
+    val list: LiveData<List<VideoInfoEntity>>
         get() = _list
 
-    private val _refresh = MutableLiveData(false)
-    val refresh: LiveData<Boolean>
-        get() = _refresh
-
-    var query=""
-
-    fun getData(start : Int){
-        if(query.isBlank()&&_refresh.value == false){
-            _refresh.value = true
-            viewModelScope.launch(Dispatchers.IO) {
-                delay(1000)
-                val tempList = list.value as MutableList
-                VideoMockData.mockList(start).forEach {
-                    tempList.add(it)
-                }
-                _list.postValue(tempList)
-                _refresh.postValue(false)
-            }
-
-        }else{
-
+    fun setQueryText(nowQuery: String?) {
+        if (nowQuery.isNullOrBlank()) {
+            _list.value = myData
+            return
         }
+
+        val regex = """.*$nowQuery.*""".toRegex()
+        val targetList = myData.filter {
+            regex.matches(it.location)
+        }
+        _list.value = targetList
     }
 
+    fun getMyData() {
+        val userInfo = getUserInfoUseCase()
+        val uid: String? = if (userInfo is Resource.Success) {
+            userInfo.result.uid
+        } else {
+            null
+        }
+
+        if (uid==null){
+            //todo network err
+        }else {
+            viewModelScope.launch {
+                val data = getMyVideoListUseCase(uid,0)
+                if(data is Resource.Success){
+                    myData = data.result
+                }else{
+                    //todo network err
+                }
+                _list.postValue(myData)
+            }
+        }
+    }
 }
