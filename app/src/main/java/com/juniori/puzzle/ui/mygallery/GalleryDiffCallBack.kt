e44f33ab25cd8e08a44bac788bfa445287fe4c26
K_Gs:D <rootachieve6053@gmail.com>
2022-11-22 15:13:10 +0900

    feat : 리스트 어댑터로 변경

--- app/src/main/java/com/juniori/puzzle/ui/mygallery/GalleryDiffCallBack.kt
+++ app/src/main/java/com/juniori/puzzle/ui/mygallery/GalleryDiffCallBack.kt
@@ -3,22 +3,12 @@ package com.juniori.puzzle.ui.mygallery
 import androidx.recyclerview.widget.DiffUtil
 import com.juniori.puzzle.domain.entity.VideoInfoEntity
 
-class GalleryDiffCallBack(private val oldItem: List<VideoInfoEntity>,
-                          private val newItem: List<VideoInfoEntity>
-) : DiffUtil.Callback() {
-    override fun getOldListSize(): Int {
-        return oldItem.size
+class GalleryDiffCallBack: DiffUtil.ItemCallback<VideoInfoEntity>() {
+    override fun areItemsTheSame(oldItem: VideoInfoEntity, newItem: VideoInfoEntity): Boolean {
+        return oldItem.videoName == newItem.videoName
     }
 
-    override fun getNewListSize(): Int {
-        return newItem.size
-    }
-
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldItem[oldItemPosition].videoName == newItem[newItemPosition].videoName
-    }
-
-    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldItem[oldItemPosition] == newItem[newItemPosition]
+    override fun areContentsTheSame(oldItem: VideoInfoEntity, newItem: VideoInfoEntity): Boolean {
+        return oldItem == newItem
     }
 }
--- app/src/main/java/com/juniori/puzzle/ui/mygallery/MyGalleryAdapter.kt
+++ app/src/main/java/com/juniori/puzzle/ui/mygallery/MyGalleryAdapter.kt
@@ -3,14 +3,13 @@ package com.juniori.puzzle.ui.mygallery
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.juniori.puzzle.databinding.ItemGalleryRecyclerBinding
 import com.juniori.puzzle.domain.entity.VideoInfoEntity
 
 
-class MyGalleryAdapter(val viewModel: MyGalleryViewModel) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    private var dataList = mutableListOf<VideoInfoEntity>()
-
+class MyGalleryAdapter(val viewModel: MyGalleryViewModel) : ListAdapter<VideoInfoEntity,MyGalleryAdapter.ViewHolder>(GalleryDiffCallBack()) {
     class ViewHolder(val binding: ItemGalleryRecyclerBinding, val height: Int) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(item: VideoInfoEntity) {
@@ -19,30 +18,17 @@ class MyGalleryAdapter(val viewModel: MyGalleryViewModel) : RecyclerView.Adapter
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
             ItemGalleryRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding, parent.height)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as ViewHolder).bind(dataList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return dataList.size
-    }
-
-    private fun calDiff(newData: List<VideoInfoEntity>) {
-        val diffCallBack = GalleryDiffCallBack(dataList, newData)
-        val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(diffCallBack)
-        diffResult.dispatchUpdatesTo(this)
-    }
-
-    fun setData(newData: List<VideoInfoEntity>) {
-        calDiff(newData)
-        dataList.clear()
-        dataList.addAll(newData)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+        if(position == itemCount - 3){
+            viewModel.getPaging(itemCount)
+        }
     }
 }
