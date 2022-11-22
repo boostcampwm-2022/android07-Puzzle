298dc54a0fd0e6d4410284d45b102027fdebd0cc
장주원 <rptrpt@ajou.ac.kr>
2022-11-22 17:14:27 +0900

    feat: playvideo activity 메뉴 구현

--- app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoActivity.kt
+++ app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
@@ -11,22 +12,20 @@ import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.Util
 import com.juniori.puzzle.R
 import com.juniori.puzzle.data.firebase.dto.VideoDetail
-import com.juniori.puzzle.databinding.ActivityPlayVideoBinding
+import com.juniori.puzzle.databinding.ActivityPlayvideoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PlayVideoActivity : AppCompatActivity() {
-    private val currentVideoItem: VideoDetail by lazy {
-        intent.extras?.get("VideoDetail") as VideoDetail
-    }
     private lateinit var exoPlayer: ExoPlayer
-    private lateinit var binding: ActivityPlayVideoBinding
-
+    private lateinit var binding: ActivityPlayvideoBinding
+    private val currentVideoItem: VideoDetail by lazy { intent.extras?.get("videoDetail") as VideoDetail }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPlayVideoBinding.inflate(layoutInflater)
+        binding = ActivityPlayvideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val uid = "xWXP9ui0RwhhS18MngcVGvz9mBf1"
+        setMenu("")
         initVideoPlayer(currentVideoItem.videoUrl.stringValue)
     }
 
@@ -43,9 +42,25 @@ class PlayVideoActivity : AppCompatActivity() {
         binding.playerView.player = exoPlayer
     }
 
+    private fun setMenu(uid: String) {
+        if (uid == currentVideoItem.ownerUid.stringValue) {
+            if (currentVideoItem.isPrivate.booleanValue) {
+                binding.materialToolbar.menu.findItem(R.id.video_private).isVisible = false
+            } else {
+                binding.materialToolbar.menu.findItem(R.id.video_public).isVisible = false
+            }
+        } else {
+            binding.materialToolbar.menu.forEach {
+                it.isVisible = false
+            }
+        }
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.play_video_menu, menu)
+        if (currentVideoItem.isPrivate.booleanValue) {
+            val inflater: MenuInflater = menuInflater
+            inflater.inflate(R.menu.playvideo_menu, menu)
+        }
         return true
     }
 }
