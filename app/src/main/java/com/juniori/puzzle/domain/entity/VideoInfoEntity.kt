d3220b7091179c0ad83f19e7ca662007fbcf3844
장주원 <rptrpt@ajou.ac.kr>
2022-11-22 17:38:19 +0900

    feat: firestore return value 수정

--- app/src/main/java/com/juniori/puzzle/domain/entity/VideoInfoEntity.kt
+++ app/src/main/java/com/juniori/puzzle/domain/entity/VideoInfoEntity.kt
@@ -2,8 +2,8 @@ package com.juniori.puzzle.domain.entity
 
 data class VideoInfoEntity(
     val ownerUid: String,
-    val videoName: String,
-    val thumbnailImage: String,
+    val videoUrl: String,
+    val thumbnailUrl: String,
     val isPrivate: Boolean,
     val likedCount: Int,
     val likedUserUidList: List<String>,
