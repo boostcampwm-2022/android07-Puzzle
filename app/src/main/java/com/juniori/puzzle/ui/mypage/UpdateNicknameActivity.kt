b15a9f880cf5330591d173e816307baa735e8b60
주재완 <yjjw2000@naver.com>
2022-11-23 18:02:01 +0900

    design : 닉네임 변경 화면 구현

---
+++ app/src/main/java/com/juniori/puzzle/ui/mypage/UpdateNicknameActivity.kt
@@ -0,0 +1,12 @@
+package com.juniori.puzzle.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juniori.puzzle.R
+
+class UpdateNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_nickname)
+    }
+}
