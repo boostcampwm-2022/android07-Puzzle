3e658b1fde84a9eec4d00cdc165e7a50923ee296
SH_H264 <jslkap227@gmail.com>
2022-11-23 09:38:48 +0900

    feat: 썸네일 추출 메소드 추가

--- app/src/main/java/com/juniori/puzzle/util/FileIOExtenstions.kt
+++ app/src/main/java/com/juniori/puzzle/util/FileIOExtenstions.kt
@@ -1,10 +1,13 @@
 package com.juniori.puzzle.util
 
 import android.content.ContentResolver
+import android.graphics.Bitmap
 import android.net.Uri
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 
+// TODO exception 처리
 fun ByteArray.saveInFile(filePath: String) {
     val file = File(filePath)
     FileOutputStream(file).use { fileOutputStream ->
@@ -15,5 +18,12 @@ fun ByteArray.saveInFile(filePath: String) {
 fun Uri.readBytes(contentResolver: ContentResolver): ByteArray? {
     return contentResolver.openInputStream(this)?.use { inputStream ->
         inputStream.readBytes()
+    }
+}
+
+fun Bitmap.compressToBytes(format: Bitmap.CompressFormat, quality: Int): ByteArray {
+    return ByteArrayOutputStream().use { outputStream ->
+        this.compress(format, quality, outputStream)
+        outputStream.toByteArray()
     }
 }
--- app/src/main/java/com/juniori/puzzle/util/VideoMetaDataUtil.kt
+++ app/src/main/java/com/juniori/puzzle/util/VideoMetaDataUtil.kt
@@ -1,18 +1,27 @@
 package com.juniori.puzzle.util
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 
-object VideoMetaDataUtil {
+class VideoMetaDataUtil(private val context: Context) {
 
-    fun getVideoDurationInSeconds(context: Context, videoUri: Uri): Long? {
+    fun getVideoDurationInSeconds(videoUri: Uri): Long? {
         return MediaMetadataRetriever().use { retriever ->
             retriever.setDataSource(context, videoUri)
             retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)
                 ?.let { milliseconds: String ->
                     milliseconds.toLong() / 1000
                 }
+        }
+    }
+
+    fun extractThumbnail(videoFilePath: String): ByteArray? {
+        return MediaMetadataRetriever().use { retriever ->
+            retriever.setDataSource(context, Uri.parse(videoFilePath))
+            retriever.getFrameAtTime(0, MediaMetadataRetriever.OPTION_CLOSEST_SYNC)
+                ?.compressToBytes(Bitmap.CompressFormat.JPEG, 100)
         }
     }
 }
