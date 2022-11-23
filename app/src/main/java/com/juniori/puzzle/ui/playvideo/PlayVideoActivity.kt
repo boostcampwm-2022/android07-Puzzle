bd7cab84c28a2336598203a8f591e247022a6c6d
장주원 <rptrpt@ajou.ac.kr>
2022-11-23 15:54:32 +0900

    feat : 비디오 제거 기능 구현

--- app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoActivity.kt
+++ app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoActivity.kt
@@ -1,31 +1,77 @@
 package com.juniori.puzzle.ui.playvideo
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEach
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.Util
+import com.google.android.material.snackbar.Snackbar
 import com.juniori.puzzle.R
+import com.juniori.puzzle.data.Resource
 import com.juniori.puzzle.databinding.ActivityPlayvideoBinding
+import com.juniori.puzzle.domain.entity.UserInfoEntity
 import com.juniori.puzzle.domain.entity.VideoInfoEntity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PlayVideoActivity : AppCompatActivity() {
-    private lateinit var exoPlayer: ExoPlayer
+
     private lateinit var binding: ActivityPlayvideoBinding
-    private val currentVideoItem: VideoInfoEntity by lazy { intent.extras?.get("videoDetail") as VideoInfoEntity }
+    private val viewModel: PlayVideoViewModel by viewModels()
+    private lateinit var exoPlayer: ExoPlayer
+    private val currentVideoItem: VideoInfoEntity by lazy { intent.extras?.get("videoInfo") as VideoInfoEntity }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayvideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val uid = "xWXP9ui0RwhhS18MngcVGvz9mBf1"
-        setMenuItems(uid)
         initVideoPlayer(currentVideoItem.videoUrl)
+        setMenuItemOnClickListener()
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.getLoginInfoFlow.collectLatest { resource ->
+                    if (resource is Resource.Success) {
+                        currentUserInfo = resource.result
+                        setMenuItems()
+                    }
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.deleteFlow.collectLatest { resource ->
+                    if (resource != null) {
+                        when (resource) {
+                            is Resource.Success -> {
+                                finish()
+                            }
+                            is Resource.Loading -> {
+                                /** loading 화면 보여주기 */
+                            }
+                            is Resource.Failure -> {
+                                resource.exception.printStackTrace()
+                                Snackbar.make(
+                                    binding.root,
+                                    "동영상 삭제에 실패했습니다.",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun initVideoPlayer(uri: String) {
@@ -41,13 +87,14 @@ class PlayVideoActivity : AppCompatActivity() {
         binding.playerView.player = exoPlayer
     }
 
-    private fun setMenuItems(uid: String) {
-        if (uid == currentVideoItem.ownerUid) {
-            if (currentVideoItem.isPrivate) {
-                binding.materialToolbar.menu.findItem(R.id.video_private).isVisible = false
-            } else {
-                binding.materialToolbar.menu.findItem(R.id.video_public).isVisible = false
-            }
+    private fun setMenuItems() {
+        if (currentUserInfo.uid == currentVideoItem.ownerUid) {
+            binding.materialToolbar.menu.findItem(R.id.video_privacy).title =
+                if (currentVideoItem.isPrivate) {
+                    getString(R.string.playvideo_to_public)
+                } else {
+                    getString(R.string.playvideo_to_private)
+                }
         } else {
             binding.materialToolbar.menu.forEach {
                 it.isVisible = false
@@ -57,18 +104,18 @@ class PlayVideoActivity : AppCompatActivity() {
 
     private fun setMenuItemOnClickListener() {
         with(binding.materialToolbar.menu) {
-            findItem(R.id.video_public).setOnMenuItemClickListener {
+            findItem(R.id.video_privacy).setOnMenuItemClickListener {
                 // Update video isPrivate to Public
                 true
             }
-            findItem(R.id.video_private).setOnMenuItemClickListener {
-                // Update video isPrivate to Private
-                true
-            }
             findItem(R.id.video_delete).setOnMenuItemClickListener {
-                // Delete video
+                viewModel.deleteVideo(currentVideoItem.documentId)
                 true
             }
         }
+    }
+
+    companion object {
+        lateinit var currentUserInfo: UserInfoEntity
     }
 }
--- app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoViewModel.kt
+++ app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoViewModel.kt
@@ -1,15 +1,41 @@
 package com.juniori.puzzle.ui.playvideo
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.juniori.puzzle.data.Resource
 import com.juniori.puzzle.data.firebase.FirestoreDataSource
 import com.juniori.puzzle.data.firebase.StorageDataSource
+import com.juniori.puzzle.domain.entity.UserInfoEntity
+import com.juniori.puzzle.domain.usecase.GetUserInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PlayVideoViewModel @Inject constructor(
+    getUserInfoUseCase: GetUserInfoUseCase,
     private val firestoreDataSource: FirestoreDataSource,
     private val storageDataSource: StorageDataSource
-): ViewModel() {
+) : ViewModel() {
+    private val _getLoginInfoFlow = MutableStateFlow<Resource<UserInfoEntity>?>(null)
+    val getLoginInfoFlow: StateFlow<Resource<UserInfoEntity>?> = _getLoginInfoFlow
 
+    private val _deleteFlow = MutableStateFlow<Resource<Unit>?>(null)
+    val deleteFlow: StateFlow<Resource<Unit>?> = _deleteFlow
+
+    init {
+        _getLoginInfoFlow.value = getUserInfoUseCase()
+    }
+
+    fun deleteVideo(documentId: String) = viewModelScope.launch {
+        _deleteFlow.emit(Resource.Loading)
+        println(documentId)
+        storageDataSource.deleteVideo(documentId).onSuccess {
+            _deleteFlow.emit(firestoreDataSource.deleteVideoItem(documentId))
+        }.onFailure {
+            _deleteFlow.emit(Resource.Failure(it as Exception))
+        }
+    }
 }
