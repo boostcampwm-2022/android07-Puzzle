9649c18407a40059373faf44b9ff75d68c72f2f6
장주원 <rptrpt@ajou.ac.kr>
2022-11-24 14:24:13 +0900

    feat : userInfoEntity에 parcelable 적용

--- app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoBottomSheet.kt
+++ app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoBottomSheet.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.juniori.puzzle.data.Resource
 import com.juniori.puzzle.databinding.BottomsheetPlayvideoBinding
 import com.juniori.puzzle.domain.entity.VideoInfoEntity
 import dagger.hilt.android.AndroidEntryPoint
@@ -35,11 +34,7 @@ class PlayVideoBottomSheet : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         val currentVideoItem = arguments?.get("videoInfo") as VideoInfoEntity
         val nickname = arguments?.get("nickName") as String
-        viewModel.getLoginInfoFlow.value?.let {
-            if (it is Resource.Success) {
-                binding.buttonOwner.text = nickname
-            }
-        }
+        binding.buttonOwner.text = nickname
         binding.buttonDate.text =
             SimpleDateFormat("yyyy-MM-dd HH:mm").format(currentVideoItem.updateTime)
         binding.buttonLocation.text = currentVideoItem.location
