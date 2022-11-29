f93a84c2fb38761ef82b5b65c912c49cb106a475
주재완 <yjjw2000@naver.com>
2022-11-29 13:46:44 +0900

    feat : 닉네임 변경 시도 후 결과 나오기 전까지 로딩화면 띄우는 기능 구현

--- app/src/main/java/com/juniori/puzzle/ui/mypage/UpdateNicknameActivity.kt
+++ app/src/main/java/com/juniori/puzzle/ui/mypage/UpdateNicknameActivity.kt
@@ -3,8 +3,6 @@ package com.juniori.puzzle.ui.mypage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.juniori.puzzle.MainActivity
@@ -12,41 +10,46 @@ import com.juniori.puzzle.R
 import com.juniori.puzzle.data.Resource
 import com.juniori.puzzle.databinding.ActivityUpdateNicknameBinding
 import com.juniori.puzzle.domain.usecase.UpdateNicknameUseCase
+import com.juniori.puzzle.util.StateManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class UpdateNicknameActivity : AppCompatActivity() {
     private val binding: ActivityUpdateNicknameBinding by lazy { ActivityUpdateNicknameBinding.inflate(layoutInflater) }
+    private val stateManager = StateManager(this)
     @Inject lateinit var updateNicknameUseCase: UpdateNicknameUseCase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        stateManager.createLoadingDialog(binding.viewContainer)
 
         binding.completeButton.setOnClickListener {
             val newNickname = binding.nicknameContainer.text.toString()
 
             if (newNickname.isBlank()) {
-                Toast.makeText(this, "닉네임을 다시 입력해주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.input_nickname_again), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
-            else {
-                lifecycleScope.launch(Dispatchers.Main.immediate) {
-                    val result = updateNicknameUseCase(newNickname)
 
-                    if (result is Resource.Success) {
-                        val intent = Intent(this@UpdateNicknameActivity, MainActivity::class.java).apply {
-                            putExtra(MyPageFragment.NEW_NICKNAME, newNickname)
-                        }
+            lifecycleScope.launch(Dispatchers.Main.immediate) {
+                stateManager.showLoadingDialog()
+                val result = withContext(Dispatchers.IO) {
+                    updateNicknameUseCase(newNickname)
+                }
+                stateManager.dismissLoadingDialog()
 
-                        setResult(RESULT_OK, intent)
-                        finish()
-                    }
-                    else {
-                        Toast.makeText(this@UpdateNicknameActivity, "닉네임을 변경할 수 없습니다!", Toast.LENGTH_SHORT).show()
-                    }
+                if (result is Resource.Success) {
+                    val intent = Intent(this@UpdateNicknameActivity, MainActivity::class.java)
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+                else {
+                    Toast.makeText(this@UpdateNicknameActivity, getString(R.string.nickname_change_impossible), Toast.LENGTH_SHORT).show()
                 }
             }
         }
