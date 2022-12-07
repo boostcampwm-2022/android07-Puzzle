bc7d83073d3da5e35f2d7c780e8dc8c805450dcf
장주원 <rptrpt@ajou.ac.kr>
2022-12-07 12:47:08 +0900

    feat: 프로그레스 다이얼로그 추가

---
+++ app/src/main/java/com/juniori/puzzle/util/ProgressDialog.kt
@@ -0,0 +1,30 @@
+package com.juniori.puzzle.util
+
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.Window
+import android.view.WindowManager
+import com.juniori.puzzle.databinding.DialogProgressBinding
+
+class ProgressDialog(context: Context) : Dialog(context) {
+    val binding = DialogProgressBinding.inflate(LayoutInflater.from(context))
+
+    init {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(binding.root)
+        setCancelable(false)
+
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun setProgress(progress: Int) {
+        binding.progressText.text = "$progress%"
+        binding.progressBar.setProgress(progress, true)
+    }
+}
