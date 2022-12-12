cd5d74061b150a2fda1b87e7562546c35b40e73d
SH_H264 <jslkap227@gmail.com>
2022-12-12 09:17:36 +0900

    fix: 잘못 고친 코드 재수정

--- app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoViewModel.kt
+++ app/src/main/java/com/juniori/puzzle/ui/playvideo/PlayVideoViewModel.kt
@@ -86,7 +86,8 @@ class PlayVideoViewModel @Inject constructor(
 
     fun deleteVideo(documentId: String) = viewModelScope.launch {
         _deleteFlow.emit(Resource.Loading)
-        _deleteFlow.emit(deleteVideoUseCase(documentId))
+        // _deleteFlow.emit(deleteVideoUseCase(documentId))
+        _deleteFlow.emit(repository.deleteVideo(documentId))
     }
 
     fun updateVideoPrivacy(documentInfo: VideoInfoEntity) = viewModelScope.launch {
