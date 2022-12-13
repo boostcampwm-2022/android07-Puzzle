74233b1e3abe0374ee41ad3df0f8489d3c135ecc
uuranus <hyur0920@gmail.com>
2022-12-13 17:48:23 +0900

    fix: 로컬 프로퍼티에 있던 변수 Const로 옮김

--- app/src/main/java/com/juniori/puzzle/data/weather/WeatherDataSourceImpl.kt
+++ app/src/main/java/com/juniori/puzzle/data/weather/WeatherDataSourceImpl.kt
@@ -1,9 +1,9 @@
 package com.juniori.puzzle.data.weather
 
-import com.juniori.puzzle.BuildConfig
 import com.juniori.puzzle.data.Resource
 import com.juniori.puzzle.domain.entity.WeatherEntity
 import com.juniori.puzzle.network.WeatherService
+import com.juniori.puzzle.util.WEATHER_SERVICE_KEY
 import com.juniori.puzzle.util.toItem
 import javax.inject.Inject
 
@@ -15,14 +15,11 @@ class WeatherDataSourceImpl @Inject constructor(
         if (lat < -90 || lat > 90 || lon < -180 || lon > 180) return Resource.Failure(Exception())
 
         return try {
-            val response = service.getWeather(lat, lon, SERVICE_KEY)
+            val response = service.getWeather(lat, lon, WEATHER_SERVICE_KEY)
             Resource.Success(response.body()?.toItem() ?: emptyList())
         } catch (e: Exception) {
             Resource.Failure(Exception())
         }
     }
 
-    companion object {
-        private const val SERVICE_KEY = BuildConfig.WEATHER_SERVICE_KEY
-    }
 }
