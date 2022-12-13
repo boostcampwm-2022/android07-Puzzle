65a89ee45717d642191f89548212f487ed089abb
uuranus <hyur0920@gmail.com>
2022-12-13 20:57:48 +0900

    fix: 날씨 영어,한국어에 맞춰서 가져오기

--- app/src/main/java/com/juniori/puzzle/data/weather/WeatherDataSourceImpl.kt
+++ app/src/main/java/com/juniori/puzzle/data/weather/WeatherDataSourceImpl.kt
@@ -5,21 +5,33 @@ import com.juniori.puzzle.domain.entity.WeatherEntity
 import com.juniori.puzzle.network.WeatherService
 import com.juniori.puzzle.util.WEATHER_SERVICE_KEY
 import com.juniori.puzzle.util.toItem
+import java.util.*
 import javax.inject.Inject
 
 class WeatherDataSourceImpl @Inject constructor(
     private val service: WeatherService
 ) : WeatherDataSource {
 
+    private val language = when (Locale.getDefault().language) {
+        "ko" -> {
+            "kr"
+        }
+        "en" -> {
+            "en"
+        }
+        else -> {
+            "ko"
+        }
+    }
+
     override suspend fun getWeather(lat: Double, lon: Double): Resource<List<WeatherEntity>> {
         if (lat < -90 || lat > 90 || lon < -180 || lon > 180) return Resource.Failure(Exception())
 
         return try {
-            val response = service.getWeather(lat, lon, WEATHER_SERVICE_KEY)
+            val response = service.getWeather(lat, lon, WEATHER_SERVICE_KEY, language)
             Resource.Success(response.body()?.toItem() ?: emptyList())
         } catch (e: Exception) {
             Resource.Failure(Exception())
         }
     }
-
 }
